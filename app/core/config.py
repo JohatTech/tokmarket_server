863fddfe2050ed5392097d9d2705918fb9ca287a
Johattech <johat.abrego24@gmail.com>
2026-08-03 20:06:01 -0500

    fixing cors

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_prefix="TOKTRADE_")
-    cors_origins: str = "http://localhost:3000"
+    cors_origins: str = "http://localhost:3000,https://tokmarket-front.vercel.app,*"
     refresh_hours: int = 24
 
     @property
